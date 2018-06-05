7dbca06f91883d9c95419658f06c7342f9b549f9
smibbie <35998407+smibbie@users.noreply.github.com>
2018-06-05 13:38:06 -0400

    adding functionality and styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,81 @@
+// Globals
+let gamesList = ["metalgearsolid", "elderscrollsonline", "callofduty",
+"supermariobros", "thewitcher", "watchdogs", "supermeatboy",
+"skyrim"];
+let chosenGame = "";
+let letters = [];
+let blanks = 0;
+let blanksAndWins = [];
+let wrongGuesses = [];
+let lettersGuessed = "";
+
+// Counters
+let wins = 0;
+let losses = 0;
+let numGuesses = 10;
+
+// Funcs
+function startGame() {
+  numGuesses = 10;
+  chosenGame = gamesList[Math.floor(Math.random() * gamesList.length)];
+  letters = chosenGame.split("");
+  blanks = letters.length;
+  blanksAndWins = [];
+  wrongGuesses = [];
+
+  for (var i = 0; i < blanks; i++) {
+    blanksAndWins.push("_");
+  }
+
+  console.log(blanksAndWins);
+  document.getElementById("guesses-left").innerHTML = numGuesses;
+  document.getElementById("word-blanks").innerHTML = blanksAndWins.join(" ");
+  document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+}
+
+function checkLetters(letter) {
+  var letterInWord = false;
+  for (var i = 0; i < blanks; i++) {
+    if (chosenGame[i] === letter) {
+      letterInWord = true;
+    }
+  }
+  if (letterInWord) {
+    for (var j = 0; j < blanks; j++) {
+      if (chosenGame[j] === letter) {
+        blanksAndWins[j] = letter;
+      }
+    }
+    console.log(blanksAndWins);
+  } else {
+    wrongGuesses.push(letter);
+    numGuesses--;
+  }
+}
+
+function roundComplete() {
+  console.log("WinCount: " + wins + " | LossCount: " + losses + " | NumGuesses: " + numGuesses);
+  document.getElementById("guesses-left").innerHTML = numGuesses;
+  document.getElementById("word-blanks").innerHTML = blanksAndWins.join(" ");
+  document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+  if (letters.toString() === blanksAndWins.toString()) {
+    wins++;
+    alert("You win!");
+    document.getElementById("win-counter").innerHTML = wins;
+    startGame();
+  }
+  else if (numGuesses === 0) {
+    losses++;
+    alert("You lose");
+    document.getElementById("loss-counter").innerHTML = losses;
+    startGame();
+  }
+}
+
+// Start game and listen for event
+startGame();
+document.onkeyup = function(event) {
+  lettersGuessed = String.fromCharCode(event.which).toLowerCase();
+  checkLetters(lettersGuessed);
+  roundComplete();
+};
